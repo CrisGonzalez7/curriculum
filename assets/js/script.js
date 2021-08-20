d753ad9c896c4e8cf3dca44a0ff9f81ee7c2b05e
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2021-08-20 18:18:45 -0400

    formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,10 @@
-(document).ready(function() {
-
-    $('.ver-mas').toggle(
-
-        // Primer click
-        function(e) {
-            $('#respuesta-ej5').slideDown();
-            $(this).text('Ocultar respuesta');
-            e.preventDefault();
-        }, // Separamos las dos funciones con una coma
-
-        // Segundo click
-        function(e) {
-            $('#respuesta-ej5').slideUp();
-            $(this).text('Ver respuesta');
-            e.preventDefault();
-        }
-
-    );
-
+$(".ver-mas, .ver-menos").click(function() {
+    $(".ver-mas").toggle();
+    $(".ocultar").toggle("slow");
 });
+
+$(".btn").click(
+    function() {
+        alert("Formulario enviado correctamente")
+    }
+)
