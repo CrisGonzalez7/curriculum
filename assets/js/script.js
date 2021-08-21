77a0927021eef35ddd706156b140cd10d7798f45
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2021-08-21 14:06:47 -0400

    cambios menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
-$(".ver-mas, .ver-menos").click(function() {
-    $(".ver-mas").toggle();
-    $(".ocultar").toggle("slow");
-});
+// Ocultar menu en mobile al hacer clic en sección
+$(".nav-link").click(
+    function() {
+        if ($("#navbarNav").hasClass("show")) {
+            $("#boton-mobile").click();
+        }
+    }
+);
 
+// Ver más, ver menos
+$(".ver-mas, .ver-menos").click(
+    function() {
+        $(".ver-mas").toggle();
+        $(".ocultar").toggle("slow");
+    }
+);
+
+// Boton enviar formulario - Alert 
 $(".btn").click(
     function() {
         alert("Formulario enviado correctamente")
     }
-)
+);
